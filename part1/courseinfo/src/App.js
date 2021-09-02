da78d023b9446ae8e1a0e09b3558b42c3b1f7361
Pedro <tampocosequeponerenesteespacio@gmail.com>
2021-09-02 16:34:03 -0400

    added courseinfo

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const Header = (props) => {
+  return (
+    <h1> 
+      {props.course}
+    </h1>
+  )
+}
+
+const Content = () => <p>Hi</p>
+
+const Total = () => <p>Hi</p>
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content  />
+      <Total  />
+    </div>
+  )
+}
+
+export default App
